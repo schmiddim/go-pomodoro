a8acb845d8057df406c74fd8579a4ea3d356289b
schmiddim <schmiddim@gmx.at>
2024-06-09 22:37:18 +0200

    Long Description

--- cmd/config.go
+++ cmd/config.go
@@ -2,16 +2,15 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/spf13/viper"
-
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 // configCmd represents the config command
 var configCmd = &cobra.Command{
 	Use:   "config",
 	Short: "Print Config",
-	Long:  `Set config with Environment Variables POMODORO_API_KEY and POMODORO_ENDPOINT`,
+	Long:  `Set config with export POMODORO_API_KEY="MYSUPERSECRETKEY"; export POMODORO_ENDPOINT="http://mykozukai-endpoint.com/api/accomplishments"`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("POMODORO_API_KEY: " + viper.GetString("POMODORO_API_KEY"))
 		fmt.Println("POMODORO_ENDPOINT: " + viper.GetString("POMODORO_ENDPOINT"))
